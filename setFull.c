38d28748acf67c5cd7d9bbb0485504cdd19b52e0
Harry Braviner <harry.braviner@gmail.com>
2015-03-29 09:14:24 +0100

    More robust version of setFull
    Previously the C program setFull would not set up the baud rate etc.,
    these would need to be first set by running e.g. screen. This has now
    been rectified for setFull only.

--- setFull.c
+++ setFull.c
@@ -1,12 +1,50 @@
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <strings.h>
+#include <unistd.h>
+
+#define BAUDRATE B9600
+#define TTYDEVICE "/dev/ttyUSB0"
+#define _POSIX_SOURCE 1
 
 int main(int argc, char *argv[]){
-	FILE *UART = fopen("/dev/ttyUSB0", "w");
 
-	fprintf(UART, "AFFFF\n");
-	fprintf(UART, "BFFFF\n");
+	int fd;	// Our tty device
+	struct termios oldtio, newtio;	// Used to hold settings
+	
+	fd = open(TTYDEVICE, O_RDWR | O_NOCTTY);
+	if(fd < 0){
+		fprintf(stderr, "Failed to open %s. Exitting.\n", TTYDEVICE);
+		return -1;
+	}
 
-	fclose(UART);
+	tcgetattr(fd, &oldtio);					// Save the current attributes
+	bzero(&newtio, sizeof(newtio));	// Wipe the new settings
+
+	newtio.c_cflag = BAUDRATE | CS8 | CLOCAL;
+
+	newtio.c_oflag = 0;				// Raw output
+	newtio.c_lflag = ICANON;	// No echo, don't send signals to calling program
+
+	tcflush(fd, TCIFLUSH);
+	tcsetattr(fd, TCSANOW, &newtio);
+
+	char *tBuf = malloc(7*sizeof(char));
+	strcpy(tBuf, "\n");
+	write(fd, tBuf, 1);
+	strcpy(tBuf, "AFFFF\n");
+	write(fd, tBuf, 6);
+	strcpy(tBuf, "BFFFF\n");
+	write(fd, tBuf, 6);
+
+	tcsetattr(fd, TCSANOW, &oldtio);
+
+	close(fd);
 
 	return 0;
 }
